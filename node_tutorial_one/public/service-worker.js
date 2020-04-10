2508e394c71a01813a6fc5ff854a78cdd6b00180
linnefromice <linnefromice@gmail.com>
2020-04-10 18:04:09 +0900

    modify service-worker.js - add function for deleting exist cache

--- node_tutorial_one/public/service-worker.js
+++ node_tutorial_one/public/service-worker.js
@@ -18,9 +18,15 @@ const toCache = [
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(toCache)
-    })
-    .then(self.skipWaiting())
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }))
+      })
+    .then(() => self.ClientRectList.claim())
   )
 });
